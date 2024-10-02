0a69f23cdd9690446e5006a4c9e8310c92a4962e
shubhamf2004 <mr.shubbhamfutane@gmail.com>
2024-10-01 21:56:28 -1200

    Some change

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express = require("express")
 const cors=require("cors")
 const mongooes=require("mongoose")
 const app=express()
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.json({limit:"10mb"}))
-const PORT=process.env.PORT || 8080
+const port=process.env.PORT || 8080
 
 //MONGODB CONNECTON
 const apiUrl = import.meta.env.VITE_API_URL;
@@ -18,4 +19,4 @@ app.post("/signup",(req,res)=>{
    console.log(req.body)
 })
 
-app.listen(PORT,()=>console.log("Server is running on "+ PORT))
+app.listen(port,()=>console.log(`Server is running on ${port}`))
