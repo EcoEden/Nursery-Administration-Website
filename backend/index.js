b950523550049865bb1f209d156c6babf12bae8e
Rushikesh Gorte <gorterushi@gmail.com>
2025-04-25 15:29:57 +0530

    Some Changes

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-const PORT = '0.0.0.0';
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
