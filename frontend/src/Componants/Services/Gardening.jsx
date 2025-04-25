6aa3d6f901ee30f89cc4d4cdf0910320f035cd2a
Rushikesh Gorte <gorterushi@gmail.com>
2025-04-26 00:19:58 +0530

    Some Change

--- frontend/src/Componants/Services/Gardening.jsx
+++ frontend/src/Componants/Services/Gardening.jsx
@@ -14,7 +14,7 @@ const GardeningEquipment = () => {
   useEffect(() => {
     const fetchEquipment = async () => {
       try {
-        const response = await axios.get(`http://13.203.120.9:5000/products?category=Gardening Equipment`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/products?category=Gardening Equipment`);
         setEquipment(response.data)
       } catch (error) {
         console.error("Error fetching equipment:", error);
